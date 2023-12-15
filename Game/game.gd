4349c0590ab648fa712881348857dd271fa0a6d4
Ruzry <35919602+Ruzry@users.noreply.github.com>
2023-12-15 15:43:10 +0000

    Game Scene Implementation
    [IMPLEMENTATION] Updated FgAnimationPlayer, Created Reveal animation, Updated Idle and Attract animations -- Created ButtonAnimationPlayer with attract animation -- Implemented game scene reset functionality -- Updated game.gd

--- Game/game.gd
+++ Game/game.gd
@@ -2,17 +2,45 @@ extends Node2D
 
 @onready var fgAnimPlayer = $FgAnimationPlayer
 @onready var bgAnimPlayer = $BgAnimationPlayer
+@onready var btnAnimPlayer = $ButtonAnimationPlayer
+
+@onready var gameStartButton = $GameStartButton
+@onready var attractTimer = $AttractTimer
+@onready var gameResetButton = $GameResetButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	resetGame()
+
+func resetGame():
 	fgAnimPlayer.play("Idle")
 	bgAnimPlayer.play("Idle")
-
+	btnAnimPlayer.play("Attract")
+	gameStartButton.visible = true
+	gameResetButton.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func _on_button_pressed():
-	pass # Replace with function body.
+func _on_timer_timeout():
+	fgAnimPlayer.play("Attract")
+
+
+func resetFgAnimation():
+	fgAnimPlayer.play("Idle")
+
+
+func _on_game_start_button_pressed():
+	fgAnimPlayer.play("Reveal")
+	gameStartButton.visible = false
+	attractTimer.stop()
+
+
+func showResetButton():
+	gameResetButton.visible = true;
+
+
+func _on_game_reset_button_pressed():
+	resetGame()
