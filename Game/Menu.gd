e00ebbbd8e1ae68d4fcde4ff8daee13c6bb3d970
Ruzry <35919602+Ruzry@users.noreply.github.com>
2023-12-22 02:15:08 +0000

    Menu & Game Scene Implementation
    [IMPLEMENTATION] Created additional menu options to choose what animal to display -- Added Menu scene animation player to swap active buttons -- Game scene now dynamically displays animal and corresponding roar based on menu option selected.

--- Game/Menu.gd
+++ Game/Menu.gd
@@ -1,28 +1,25 @@
 extends Control
 
+var gameScene = preload("res://game.tscn").instantiate()
 @onready var sfxAudio = $SfxAudio
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@onready var bgAudio = $BackgroundAudio
+@onready var menuAnimationPlayer = $MenuAnimationPlayer
 
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	menuAnimationPlayer.play("AnimalSelection")
+
+
+func startGameScene(animalIndex):
+	gameScene.setAnimalIndex(animalIndex)
+	bgAudio.stop()
+	get_tree().root.add_child(gameScene)
 
 
 func _on_exit_button_pressed():
 	get_tree().quit()
 
 
-func _on_play_button_button_down():
+func playButtonPressSound():
 	sfxAudio.play()
 
-
-func _on_exit_button_button_down():
-	sfxAudio.play()
--- Game/game.gd
+++ Game/game.gd
@@ -1,17 +1,50 @@
 extends Node2D
 
+enum ANIMAL_TYPE { LION, ELEPHANT, GORILLA}
+
+#Animation Players
 @onready var fgAnimPlayer = $FgAnimationPlayer
 @onready var bgAnimPlayer = $BgAnimationPlayer
 @onready var btnAnimPlayer = $ButtonAnimationPlayer
-
+#General Nodes
 @onready var gameStartButton = $GameStartButton
 @onready var attractTimer = $AttractTimer
 @onready var gameResetButton = $GameResetButton
-
+#Lion Nodes
+@onready var lionHeadSprite = $Animal/Head/LionHead
+@onready var lionLeftPawSprite = $Animal/LeftPaw/LionLeftPaw
+@onready var lionRightPawSprite = $Animal/RightPaw/LionRightPaw
+#Elephant Nodes
+@onready var elephantHeadSprite = $Animal/Head/ElephantHead
+@onready var elephantBodySprite = $Animal/Body/ElephantBody
+@onready var elephantLeftPawSprite = $Animal/LeftPaw/ElephantLeftPaw
+@onready var elephantRightPawSprite = $Animal/RightPaw/ElephantRightPaw
+#Gorilla nodes
+@onready var gorillaHeadSprite = $Animal/Head/GorillaHead
+@onready var gorillaLeftPawSprite = $Animal/LeftPaw/GorillaLeftPaw
+@onready var gorillaRightPawSprite = $Animal/RightPaw/GorillaRightPaw
+#Audio
 @onready var BgAudio = $BGAudio
+@onready var animalRoarAudio = $AnimalRoarAudio
+@onready var lionRoar = preload("res://Assets/Sound/SFX/Jungle Peek A Boo Lion Reveal sfx.wav")
+@onready var elephantRoar = preload("res://Assets/Sound/SFX/Jungle Peek A Boo Elephant Reveal sfx.wav")
+@onready var gorillaRoar = preload("res://Assets/Sound/SFX/Jungle Peek A Boo Monkey Ambience sfx.wav")
+
+#Animal node Arrays
+var lionNodeSprites: Array
+var elephantNodeSprites: Array
+var gorillaNodeSprites: Array
+
+var animalIndex: int
+var animalAudioStreams: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	lionNodeSprites = [lionHeadSprite, lionLeftPawSprite, lionRightPawSprite]
+	elephantNodeSprites = [elephantHeadSprite, elephantBodySprite, elephantLeftPawSprite, elephantRightPawSprite]
+	gorillaNodeSprites = [gorillaHeadSprite, gorillaLeftPawSprite, gorillaRightPawSprite]
+	animalAudioStreams = [lionRoar, elephantRoar, gorillaRoar]
+	setAnimalVisiblity()
 	resetGame()
 
 func resetGame():
@@ -26,6 +59,21 @@ func _process(delta):
 	pass
 
 
+func setAnimalIndex(animalIndex_: int):
+	animalIndex = animalIndex_
+
+
+func setAnimalVisiblity():
+	for n in lionNodeSprites.size():
+		lionNodeSprites[n].visible = true if (animalIndex == ANIMAL_TYPE.LION) else false
+		
+	for n in elephantNodeSprites.size():
+		elephantNodeSprites[n].visible = true if (animalIndex == ANIMAL_TYPE.ELEPHANT) else false
+		
+	for n in gorillaNodeSprites.size():
+		gorillaNodeSprites[n].visible = true if (animalIndex == ANIMAL_TYPE.GORILLA) else false
+
+
 func _on_timer_timeout():
 	fgAnimPlayer.play("Attract")
 
@@ -33,8 +81,10 @@ func _on_timer_timeout():
 func resetFgAnimation():
 	fgAnimPlayer.play("Idle")
 
-func revealAnimationStarted():
-	pass
+
+func setRoarAudioStream():
+	animalRoarAudio.stream = animalAudioStreams[animalIndex]
+
 
 func _on_game_start_button_pressed():
 	fgAnimPlayer.play("Reveal")
@@ -48,3 +98,4 @@ func showResetButton():
 
 func _on_game_reset_button_pressed():
 	resetGame()
+
